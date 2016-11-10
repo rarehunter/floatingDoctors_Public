d554cbfc3c3d160e77968148392578f12b8caaf9
amy <yiming3478@gmail.com>
2016-11-10 14:39:16 -0500

    Setup the boilderplate for react-d3 project

---
+++ src/index.jsx
@@ -0,0 +1,27 @@
+import { AppContainer } from 'react-hot-loader';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import d3 from 'd3';
+import App from './App.jsx';
+
+const rootEl = document.getElementById('root');
+ReactDOM.render(
+  <AppContainer>
+    <App />
+  </AppContainer>,
+  rootEl
+);
+
+if (module.hot) {
+  module.hot.accept('./App.jsx', () => {
+    // If you use Webpack 2 in ES modules mode, you can
+    // use <App /> here rather than require() a <NextApp />.
+    const NextApp = require('./App.jsx').default;
+    ReactDOM.render(
+      <AppContainer>
+         <NextApp />
+      </AppContainer>,
+      rootEl
+    );
+  });
+}

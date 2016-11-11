43d223085f42dc781e1802450b3330a8e74cc383
amy pu <yiming3478@gmail.com>
2016-11-10 19:27:22 -0500

    Tried to modularize css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import styles from './app.css';
 import React, { Component } from 'react';
 import Layout from './Layout.jsx';
 import Counter from './Counter.jsx';
+import Chart from './chart.jsx';
 
 export default class App extends Component {
   render() {
     return (
       <Layout>
+        <h1 className="{styles.btn}">Hello</h1>
         <Counter />
+        <Chart />
       </Layout>
     );
   }
---
+++ src/chart.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const styles = {
+	width: 500,
+	height: 300,
+	padding: 30
+}
+
+const numDataPoints = 50;
+
+const randomNum = () => Math.floor(Math.random() * 10000);
+
+const randomDataSet = () => {
+	return Array.apply(null, {length: numDataPoints}).map(() => [randomNum(), randomNum()]);
+}
+
+export default class Chart extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {data: randomDataSet()};
+	}
+
+	randomizeData() {
+		this.setState({data: randomDataSet()});
+	}
+
+	render() {
+		return <div>
+			<h1>React & D3</h1>
+			<div className="controls">
+				<button className="btn randomize" onClick={() => this.randomizeData()}>
+					Randomize data
+				</button>
+			</div>
+		</div>
+	}
+}

af1a3d322a4a1d7045b3995e77b5f69f1a81928d
Samuel Cheng <rarehunter@users.noreply.github.com>
2016-11-19 10:58:14 -0500

    Added Modal component to be used - can change as needed

--- src/components/FDModal.jsx
+++ src/components/FDModal.jsx
@@ -21,31 +21,38 @@ export default class FDModal extends React.Component {
     }
 
     render() {
-      return (
-        <ButtonToolbar>
-          <Button bsStyle="primary" onClick={this.showModal}>
-            Launch demo modal
-          </Button>
+        return (
+            <ButtonToolbar>
+                <Button bsStyle="primary" onClick={this.showModal}>
+                    Some Community
+                </Button>
 
-          <Modal
-            {...this.props}
-            show={this.state.show}
-            onHide={this.hideModal}
-            dialogClassName="custom-modal"
-          >
-            <Modal.Header closeButton>
-              <Modal.Title id="contained-modal-title-lg">Modal heading</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <h4>Wrapped Text</h4>
-              <p>Blah</p>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={this.hideModal}>Close</Button>
-            </Modal.Footer>
-          </Modal>
-        </ButtonToolbar>
-        );
+                <Modal
+                    {...this.props}
+                    show={this.state.show}
+                    onHide={this.hideModal}
+                    dialogClassName="custom-modal"
+                >
+
+                    <Modal.Header closeButton>
+                        <Modal.Title id="contained-modal-title-lg">Community Name Here</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <h4>Some header here</h4>
+                        <p>Place stuff here</p>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button onClick={this.hideModal}>Close</Button>
+                    </Modal.Footer>
+
+                </Modal>
+
+            </ButtonToolbar>
+
+        ); // end return statement
+
     } // end render function
 
 } // end export default

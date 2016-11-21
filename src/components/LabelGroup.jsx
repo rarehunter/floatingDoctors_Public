b58d1b30129d81709cde58bd933c1c865db1a12d
amy <yiming3478@gmail.com>
2016-11-21 10:43:12 -0500

    Fixed a typo

--- src/components/LabelGroup.jsx
+++ src/components/LabelGroup.jsx
@@ -6,7 +6,7 @@ export default class LabelGroup extends React.Component {
 
 	render() {
 		return (
-			<text className="styles.labelTitle" x={this.props.x} y={this.props.y}>
+			<text className={styles.labelTitle} x={this.props.x} y={this.props.y}>
 				{this.props.title}
 				{this.props.labels && this.props.labels.map((l, i) => {
 					return (

49bda003fdf32cc9d337e4df5bcf9e764094abad
amy <yiming3478@gmail.com>
2016-12-08 02:42:38 -0500

    Fixed the loading screen issue

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -5,17 +5,19 @@ export default function Loading(props) {
 
 	return (
 		<div className={styles.loader}>
-			<div className={styles.loaderInnerText}>
-				<label>In Latin America and the Caribbean, about <span style={{fontWeight:'600'}}>30%</span> do not have access to healthcare for economic reasons, and <span style={{fontWeight:'600'}}>21%</span> do not seek care because of geographical barriers.</label>
-				<label><span style={{fontWeight:'600'}}>The Floating Doctors</span>, a non-profit organization, delivers health care services to coastal remote communities of Central America.</label>
-			</div>
-			<div className={styles.loaderInner}>
-				<label>	●</label>
-				<label>	●</label>
-				<label>	●</label>
-				<label>	●</label>
-				<label>	●</label>
-				<label>	●</label>
+			<div className={styles.loaderInnerWrap}>
+				<div className={styles.loaderInnerText}>
+					<label>In Latin America and the Caribbean, about <span style={{fontWeight:'600'}}>30%</span> do not have access to healthcare for economic reasons, and <span style={{fontWeight:'600'}}>21%</span> do not seek care because of geographical barriers.</label>
+					<label><span style={{fontWeight:'600'}}>The Floating Doctors</span>, a non-profit organization, delivers health care services to coastal remote communities of Central America.</label>
+				</div>
+				<div className={styles.loaderInner}>
+					<label>	●</label>
+					<label>	●</label>
+					<label>	●</label>
+					<label>	●</label>
+					<label>	●</label>
+					<label>	●</label>
+				</div>
 			</div>
 		</div>
 	);
